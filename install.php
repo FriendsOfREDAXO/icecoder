53d019275fe9d98cf2c481bba34930a59afe7317
Markus Staab <markus.staab@redaxo.de>
2018-09-22 11:40:22 +0200

    Individualisierte Ordnerstrukturen unterstützen

--- install.php
+++ install.php
@@ -1,3 +1,3 @@
 <?php
 
-rex_dir::copy(rex_path::backend('src/addons/icecoder/vendor/icecoder'), '../icecoder');
+rex_dir::copy(rex_path::backend('src/addons/icecoder/vendor/icecoder'), rex_path::frontend('icecoder'));
