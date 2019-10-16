426c086197985ea1e8510ea19511db570dfc1d60
Thomas Skerbis <thomas.skerbis@mac.com>
2019-10-16 21:15:37 +0200

    Update update.php

--- update.php
+++ update.php
@@ -1,4 +1,4 @@
 <?php
 if (rex_string::versionCompare($this->getVersion(), '2.0', '<')) {
-throw new rex_functional_exception('Please uninstall old version and start a new installation');
+throw new rex_functional_exception('Please uninstall old version and install the new version');
 }
