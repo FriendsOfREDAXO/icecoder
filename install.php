fb6be8bec29b6cfdd4c9524e0db6fe45d1a242b8
Thomas Skerbis <thomas.skerbis@mac.com>
2021-11-22 14:28:50 +0100

    Update install.php

--- install.php
+++ install.php
@@ -9,7 +9,7 @@ rex_dir::copy($this->getPath('vendor/icecoder'), rex_path::frontend('icecoder'))
 
 // resolve the HOST at installation time, so we dont need to resolve it at login-time.
 // this makes sure noone can fake the auto-login url.
-
+// replaces the vendor index.php
 $index = rex_file::get($this->getPath('ice_index.php'));
 $index = str_replace('%%HOST%%', $_SERVER['HTTP_HOST'], $index);
 rex_file::put(rex_path::frontend('icecoder/index.php'), $index);
